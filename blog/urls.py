0dd4c7686c7c1d49afb616ec41681c8b19441708
anahita83radfar <anahita83radfar@gmail.com>
2022-06-20 10:45:57 +0000

    Add update and delete post view

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
     path('new/', views.PostCreate.as_view(), name='post_create'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('<slug:slug>/delete/', views.PostDelete.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from .forms import CommentForm
 
@@ -79,16 +80,44 @@ class PostLike(View):
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
     
 
-class PostCreate(generic.CreateView):
+class PostCreate(LoginRequiredMixin, generic.CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'status']
+    template_name = "post_form.html"
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    fields = ['title', 'content', 'status']
     template_name = "post_form.html"
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         print(form)
         return super().form_valid(form)
-
     
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+class PostDelete(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    template_name = "post_confirm_delete.html"
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
